c2528be14d0b84936856a8774e69e4324dfc7bf1
Cody <simoncody121@gmail.com>
2023-09-25 23:43:33 -0500

    img sizing

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -25,8 +25,8 @@ function App() {
   return (
     <div>
       <Navbar />
-    <div className="main min-h-screen grid grid-cols-2 gap-4 place-content-center h-48 glass">
-    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 gap-10 px-9 gap-y-20 py-9">
+    <div className="main min-h-screen grid grid-cols-2 gap-4 place-content-center h-48">
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 gap-10 px-9 gap-y-20 py-9 glass">
       {pics.map((pic, i) => {
         return (
           <div className="relative" key={i}>
